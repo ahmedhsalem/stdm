df57a2c4b28848c75287095c345ff7f427e95138
ahmedhsalem <32573965+ahmedhsalem@users.noreply.github.com>
2020-05-08 16:58:37 +0100

    Update maptools.R

--- maptools.R
+++ maptools.R
@@ -1,3 +1,50 @@
+library(tmap)
+library(tmaptools)
+library(ggplot2)
+library(maptools)
+library(lattice)
+library(spdep)
+library(sp)
+library(rgdal)
+library(gridExtra)
+library(gstat)
+library(OpenStreetMap)
+library(spacetime)
+library(ggpmisc)
+xmin<- -0.15279
+ymin<- 51.51260
+xmax<- -0.12221
+ymax<- 51.52266
+
+bboxt <- c(xmin = xmin, ymin = ymin, xmax = xmax, ymax = ymax)
+tm_shape(LCAPShp, bbox=bboxt)+tm_lines()+tm_scale_bar()+tm_grid(labels.size =0.6)+tm_text("LCAP_ID",size=0.9, remove.overlap = TRUE)
+ttm()
+
+hist(LCAPShp@data$SPEED)
+data<-LCAPShp@data
+
+#Plotting Functions
+
+focusLCAPIDs<-c(1883,1884,420,423,1576,1593,1613,1616,1412,1413,2112,2364,2363,2173,2318,2433,435,524,437,469,425,463)
+focusShapeMatrix<-matrix(,nrow=0,ncol=42)
+
+for(ID in focusLCAPIDs)
+  focusShapeMatrix<-rbind(focusShapeMatrix,subset(data,LCAP_ID==ID))
+
+UJTFocusMatrix<-subset(UJT,select=as.character(focusLCAPIDs))
+AdjFocusMatrix<-subset(LCAPAdj,select=as.character(focusLCAPIDs))
+
+
+merge(UJTFocusMatrix,dates)
+AnalysisTable<-cbind(dates,UJTFocusMatrix)
+
+avg<-colMeans(UJT[,])
+
+avgfocus<-rowMeans(AnalysisTable[,3:ncol(AnalysisTable)])
+
+rowsbyDate<-aggregate(avgfocus,by=list(Date=AnalysisTable$Date),FUN=max)
+rowsbyTime<-aggregate(avgfocus,by=list(Time=AnalysisTable$Time),FUN=max)
+#rowsbyWeek<-apply.weekly(avgfocus,mean)
 plot(rowsbyDate)
 ggplot(rowsbyDate, aes(x=Date, y=x))+geom_point()+geom_smooth(method="lm")+
   stat_poly_eq(formula = y~x, 
@@ -29,7 +76,6 @@ Weights<-mat2listw(LCAPAdj, style="M")
 moran.test(x=avg, listw=Weights,zero.policy = TRUE)
 
 #Semi-Variogram
-
 avgfocuscol<-colMeans(UJTFocusMatrix)
 a<-LCAPShp@data[,1:2]
 b<-ggplot2::fortify(LCAPShp)
